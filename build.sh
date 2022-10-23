991d84070ea15a31d85a2965d974596ef1be8533
ssandrini2 <ssandrini@itba.edu.ar>
2022-10-23 16:22:39 -0300

    build.sh

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mvn clean install
+cd server/target
+tar -xvf tpe2-g9-server-1.0-SNAPSHOT-bin.tar.gz
+cd tpe2-g9-server-1.0-SNAPSHOT
+chmod u+x run-server.sh
+
+
+cd ..
+cd ..
+cd ..
+cd client/target
+tar -xvf tpe2-g9-client-1.0-SNAPSHOT-bin.tar.gz
+cd tpe2-g9-client-1.0-SNAPSHOT
+chmod u+x query1
+
+
