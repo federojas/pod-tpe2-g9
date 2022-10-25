3e529e1071c903a5f3fce63896a0dc090892fa39
Leonardo <ldagostino@itba.edu.ar>
2022-10-24 23:38:16 -0300

    Better setUp Testing

--- client/src/test/java/Q1Test.java
+++ client/src/test/java/Q1Test.java
@@ -1,10 +1,12 @@
 import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.client.test.TestHazelcastFactory;
-import com.hazelcast.config.Config;
-import com.hazelcast.config.GroupConfig;
+import com.hazelcast.config.*;
 import com.hazelcast.core.HazelcastInstance;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Collections;
 
 public class Q1Test {
     private HazelcastInstance client, member;
@@ -12,15 +14,35 @@ public class Q1Test {
 
     @Before
     public void setUp() {
-        String name = "g9", pass = "g9-pass";
+        String ip = "192.168.0.*", name = "g9", pass = "g9-pass";
         hzFactory = new TestHazelcastFactory();
-        client = hzFactory.newHazelcastClient();
 
+        Config config = new Config();
         GroupConfig groupConfig = new GroupConfig().setName(name).setPassword(pass);
-        Config config = new Config().setGroupConfig(groupConfig);
+        config.setGroupConfig(groupConfig);
+
+        MulticastConfig multicastConfig = new MulticastConfig();
+        JoinConfig joinConfig = new JoinConfig().setMulticastConfig(multicastConfig);
+
+        InterfacesConfig interfacesConfig = new InterfacesConfig()
+                .setInterfaces(Collections.singletonList(ip))
+                .setEnabled(true);
+
+        NetworkConfig networkConfig = new NetworkConfig()
+                .setInterfaces(interfacesConfig)
+                .setJoin(joinConfig);
+
+        config.setNetworkConfig(networkConfig);
+
         member = hzFactory.newHazelcastInstance(config);
+        
         ClientConfig clientConfig = new ClientConfig().setGroupConfig(groupConfig);
         client = hzFactory.newHazelcastClient(clientConfig);
+    }
+
+    @Test
+    public void test() {
+        // TODO
     }
 
     @After
