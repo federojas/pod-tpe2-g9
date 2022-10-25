99df25a3a310eda8a52c7c5a0dd7644e7906b875
Leonardo <ldagostino@itba.edu.ar>
2022-10-24 23:47:09 -0300

    QueryTest parent class modularization

--- client/src/test/java/Q1Test.java
+++ client/src/test/java/Q1Test.java
@@ -8,45 +8,15 @@ import org.junit.Test;
 
 import java.util.Collections;
 
-public class Q1Test {
+public class Q1Test extends QueryTest {
     private HazelcastInstance client, member;
     private TestHazelcastFactory hzFactory;
 
-    @Before
-    public void setUp() {
-        String ip = "192.168.0.*", name = "g9", pass = "g9-pass";
-        hzFactory = new TestHazelcastFactory();
-
-        Config config = new Config();
-        GroupConfig groupConfig = new GroupConfig().setName(name).setPassword(pass);
-        config.setGroupConfig(groupConfig);
-
-        MulticastConfig multicastConfig = new MulticastConfig();
-        JoinConfig joinConfig = new JoinConfig().setMulticastConfig(multicastConfig);
-
-        InterfacesConfig interfacesConfig = new InterfacesConfig()
-                .setInterfaces(Collections.singletonList(ip))
-                .setEnabled(true);
-
-        NetworkConfig networkConfig = new NetworkConfig()
-                .setInterfaces(interfacesConfig)
-                .setJoin(joinConfig);
-
-        config.setNetworkConfig(networkConfig);
-
-        member = hzFactory.newHazelcastInstance(config);
-        
-        ClientConfig clientConfig = new ClientConfig().setGroupConfig(groupConfig);
-        client = hzFactory.newHazelcastClient(clientConfig);
-    }
 
     @Test
     public void test() {
         // TODO
     }
 
-    @After
-    public void closeHazelcast() {
-        hzFactory.shutdownAll();
-    }
+
 }
